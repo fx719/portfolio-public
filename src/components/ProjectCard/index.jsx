6847baa9bb0db5b55fe747722e15772472fbabf0
fx719 <frederic.dupont78@yahoo.fr>
2026-07-13 16:00:55 +0200

    style (components): add glass texture to the ProjectCard component

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -3,10 +3,12 @@ import "./ProjectCard.css"
 
 export default function ProjectCard({ projectUrl, projectName, backgroundImgUrl }) {
     return (
-        <a target="_blank" href={projectUrl} className="projectCard" style={{ backgroundImage: `url(${backgroundImgUrl})` }}>
-
-            <h3 className="projectName">{projectName}</h3>
-
-        </a>
+        <>
+            <a target="_blank" href={projectUrl} className="projectCard" style={{ backgroundImage: `url(${backgroundImgUrl})` }}>
+                <h3 className="projectName">{projectName}</h3>
+                <div className="glass_cover">
+                </div>
+            </a>
+        </>
     )
 }
