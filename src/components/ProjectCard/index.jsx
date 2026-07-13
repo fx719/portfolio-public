e63996ca6c5756300829610aa5d9dc91c4629bf4
fx719 <frederic.dupont78@yahoo.fr>
2026-07-13 11:08:27 +0200

    style(pages): change the Home page display structure also prepare the projectCard component for background implementation

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,10 +1,10 @@
 import "./ProjectCard.css"
 
 
-export default function ProjectCard({ projectUrl, projectName }) {
+export default function ProjectCard({ projectUrl, projectName, backgroundImgUrl }) {
     return (
-        <a target="_blank" href={projectUrl}>
-            <div className="projectCard">
+        <a target="_blank" href={projectUrl} className="projectCard">
+            <div style={{ background: backgroundImgUrl }}>
                 <h3>{projectName}</h3>
             </div>
         </a>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,21 @@
 import ProjectCard from "../../components/ProjectCard"
+import bookiLogo from '../../assets/Booki.png'
 import "./Home.css"
 
 export default function Home() {
     return (
         <div className="projectsGallery">
-            <div className="projects-1-2">
-                <ProjectCard projectUrl="https://fx719.github.io/booki/" projectName="Booki" />
-                <ProjectCard projectUrl="https://github.com/fx719/Portfolio-architecte-sophie-bluel/tree/master/FrontEnd" projectName="Sophie Bluel" />
-            </div>
-            <div className="projects-sideProject1-sideProject2-5">
-                <ProjectCard projectName="Actu des jeux" />
+            <div className="side_projects">
                 <ProjectCard projectName="Homemade Sutom" />
+                <ProjectCard projectName="Actu des jeux" />
+            </div>
+            <div className="gh_pages_oc_projects">
+                <ProjectCard projectUrl="https://fx719.github.io/booki/" projectName="Booki" backgroundImgUrl={bookiLogo} />
+                <ProjectCard projectUrl="https://fx719.github.io/Nina-Carducci-Dev-master/" projectName="Nina Carducci" />
                 <ProjectCard projectUrl="https://github.com/fx719/kasa" projectName="Kasa" />
             </div>
-            <div className="projects-3-4">
-                <ProjectCard projectUrl="https://fx719.github.io/Nina-Carducci-Dev-master/" projectName="Nina Carducci" />
+            <div className="gh_code_oc_only">
+                <ProjectCard projectUrl="https://github.com/fx719/Portfolio-architecte-sophie-bluel/tree/master/FrontEnd" projectName="Sophie Bluel" />
                 <ProjectCard projectUrl="https://github.com/fx719/monVieuxGrimoire-backend" projectName="Mon Vieux Grimoire" />
             </div>
         </div>
