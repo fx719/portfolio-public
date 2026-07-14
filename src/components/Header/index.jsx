9b3d2c51741a22744b5c9d56f366143cffe0dcd9
fx719 <frederic.dupont78@yahoo.fr>
2026-07-14 11:21:49 +0200

    style(components): adjust Header font-size for <=400px width

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,7 +7,7 @@ export default function Header() {
         <header>
             <div className="header-name">
                 <NavLink to="/"><h2>Frédéric Dupont</h2></NavLink>
-                <NavLink to="/bio"><p>+ d'infos</p></NavLink>
+                <NavLink to="/bio"><p>À propos</p></NavLink>
             </div>
             <div className="header-skills">
                 <h2>Développeur web</h2>
