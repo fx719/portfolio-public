1b592027a27f15de84f4c99f20cc99e718e0b6ba
fx719 <frederic.dupont78@yahoo.fr>
2026-07-16 21:50:32 +0200

    fix(components): 2nd attempt to fix cloudflare pages images issue in the Project Card component

--- src/data/projectsPicturesPaths.js
+++ src/data/projectsPicturesPaths.js
@@ -1,13 +1,34 @@
+import bookiPicture1 from "../assets/projectPictures/Booki/booki_1.webp"
+import bookiPicture2 from "../assets/projectPictures/Booki/booki_2.webp"
+import bookiPicture3 from "../assets/projectPictures/Booki/booki_3.webp"
+import hsPicture1 from "../assets/projectPictures/homemadeSutom/hs_1.webp"
+import hsPicture2 from "../assets/projectPictures/homemadeSutom/hs_2.webp"
+import hsPicture3 from "../assets/projectPictures/homemadeSutom/hs_3.webp"
+import kasaPicture1 from "../assets/projectPictures/kasa/kasa_1.webp"
+import kasaPicture2 from "../assets/projectPictures/kasa/kasa_2.webp"
+import kasaPicture3 from "../assets/projectPictures/kasa/kasa_3.webp"
+import kasaPicture4 from "../assets/projectPictures/kasa/kasa_4.webp"
+import mvgPicture1 from "../assets/projectPictures/monVieuxGrimoire/mvg_1.webp"
+import mvgPicture2 from "../assets/projectPictures/monVieuxGrimoire/mvg_2.webp"
+import mvgPicture3 from "../assets/projectPictures/monVieuxGrimoire/mvg_3.webp"
+import ncPicture1 from "../assets/projectPictures/ninaCarducci/nc_1.webp"
+import ncPicture2 from "../assets/projectPictures/ninaCarducci/nc_2.webp"
+import ncPicture3 from "../assets/projectPictures/ninaCarducci/nc_3.webp"
+import ncPicture4 from "../assets/projectPictures/ninaCarducci/nc_4.webp"
+import sbPicture1 from "../assets/projectPictures/sophieBluel/sb_1.webp"
+import sbPicture2 from "../assets/projectPictures/sophieBluel/sb_2.webp"
+import sbPicture3 from "../assets/projectPictures/sophieBluel/sb_3.webp"
+import sbPicture4 from "../assets/projectPictures/sophieBluel/sb_4.webp"
+import sbPicture5 from "../assets/projectPictures/sophieBluel/sb_5.webp"
 
-const pathPrefix = "/assets/projectPictures/"
 
 const projectsPicturesPaths = {
-    booki: [pathPrefix + "Booki/booki_1.webp", pathPrefix + "Booki/booki_2.webp", pathPrefix + "Booki/booki_3.webp"],
-    homemadeSutom: [pathPrefix + "homemadeSutom/hs_1.webp", pathPrefix + "homemadeSutom/hs_2.webp", pathPrefix + "homemadeSutom/hs_3.webp"],
-    kasa: [pathPrefix + "kasa/kasa_1.webp", pathPrefix + "kasa/kasa_2.webp", pathPrefix + "kasa/kasa_3.webp", pathPrefix + "kasa/kasa_4.webp"],
-    monVieuxGrimoire: [pathPrefix + "monVieuxGrimoire/mvg_1.webp", pathPrefix + "monVieuxGrimoire/mvg_2.webp", pathPrefix + "monVieuxGrimoire/mvg_3.webp"],
-    ninaCarducci: [pathPrefix + "ninaCarducci/nc_1.webp", pathPrefix + "ninaCarducci/nc_2.webp", pathPrefix + "ninaCarducci/nc_3.webp", pathPrefix + "ninaCarducci/nc_4.webp"],
-    sophieBluel: [pathPrefix + "sophieBluel/sb_1.webp", pathPrefix + "sophieBluel/sb_2.webp", pathPrefix + "sophieBluel/sb_3.webp", pathPrefix + "sophieBluel/sb_4.webp", pathPrefix + "sophieBluel/sb_5.webp"]
+    booki: [bookiPicture1, bookiPicture2, bookiPicture3],
+    homemadeSutom: [hsPicture1, hsPicture2, hsPicture3],
+    kasa: [kasaPicture1, kasaPicture2, kasaPicture3, kasaPicture4],
+    monVieuxGrimoire: [mvgPicture1, mvgPicture2, mvgPicture3],
+    ninaCarducci: [ncPicture1, ncPicture2, ncPicture3, ncPicture4],
+    sophieBluel: [sbPicture1, sbPicture2, sbPicture3, sbPicture4, sbPicture5]
 }
 
 export default projectsPicturesPaths
--- src/data/stacks.js
+++ src/data/stacks.js
@@ -1,13 +1,25 @@
+import adonisjsLogo from "../assets/techPictures/adonis.svg"
+import cssLogo from "../assets/techPictures/css-brands-solid-full.svg"
+import htmlLogo from "../assets/techPictures/html5-brands-solid-full.svg"
+import typescriptLogo from "../assets/techPictures/typescript-brands-solid-full.svg"
+import javascriptLogo from "../assets/techPictures/js-brands-solid-full.svg"
+import mysqlLogo from "../assets/techPictures/powered-by-mysql-125x64.png"
+import sassLogo from "../assets/techPictures/sass-brands-solid-full.svg"
+import reactLogo from "../assets/techPictures/react-brands-solid-full.svg"
+import expressLogo from "../assets/techPictures/express.svg"
+import nodejsLogo from "../assets/techPictures/node-brands-solid-full.svg"
+import mongodbLogo from "../assets/techPictures/MongoDB_Spring-Green.svg"
 
-const pathPrefix = "/assets/techPictures/"
+
+
 
 const stacks = {
-    booki: [pathPrefix + "html5-brands-solid-full.svg", pathPrefix + "css-brands-solid-full.svg"],
-    homemadeSutom: [pathPrefix + "typescript-brands-solid-full.svg", pathPrefix + "adonis.svg", pathPrefix + "js-brands-solid-full.svg"],
-    ninaCarducci: [pathPrefix + "js-brands-solid-full.svg", pathPrefix + "html5-brands-solid-full.svg", pathPrefix + "css-brands-solid-full.svg"],
-    kasa: [pathPrefix + "sass-brands-solid-full.svg", pathPrefix + "react-brands-solid-full.svg"],
-    sophieBluel: [pathPrefix + "js-brands-solid-full.svg", pathPrefix + "html5-brands-solid-full.svg", pathPrefix + "css-brands-solid-full.svg"],
-    monVieuxGrimoire: [pathPrefix + "node-brands-solid-full.svg", pathPrefix + "express.svg", pathPrefix + "MongoDB_Spring-Green.svg"]
+    booki: [htmlLogo, cssLogo],
+    homemadeSutom: [typescriptLogo, adonisjsLogo, javascriptLogo, mysqlLogo],
+    ninaCarducci: [javascriptLogo, htmlLogo, cssLogo],
+    kasa: [sassLogo, reactLogo],
+    sophieBluel: [javascriptLogo, htmlLogo, cssLogo],
+    monVieuxGrimoire: [nodejsLogo, expressLogo, mongodbLogo]
 }
 
 
