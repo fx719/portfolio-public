89aeb5e888aec4e3bd7e91a046a75888eb4a4054
fx719 <frederic.dupont78@yahoo.fr>
2026-07-16 22:37:44 +0200

    style(components): add responsive style to the closeModalButton in the Project card component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <header>
             <div className="header-name">
-                <NavLink to="/"><h2>Frédéric Dupont</h2></NavLink>
+                <NavLink to="/"><h2>Frédéric Dupont (acceuil)</h2></NavLink>
                 <NavLink to="/bio"><p>À propos</p></NavLink>
                 <a href="https://www.linkedin.com/in/fredericdupont75/" id='linkedin'>Contact</a>
             </div>
