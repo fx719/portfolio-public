116510a4f3174f6cd41ab317b37fe5d48b9d91d0
fx719 <frederic.dupont78@yahoo.fr>
2026-07-17 17:54:11 +0200

    feat(components): add focus trap for keyboard-navigation in the Project card component

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -6,22 +6,49 @@ export default function ProjectCard({ projectUrl, projectName, backgroundImgUrl,
     const modalRef = useRef(null)
     const zoomedPictureRef = useRef(null)
 
+
+
+    let focusableElements = []
+
+
+
+    const getFocusableElements = (modal) => {
+        const focusableSelector = "button, img"
+        focusableElements = Array.from(modal.querySelectorAll(focusableSelector))
+        return focusableElements
+    }
+
+
+
+
+
     const openModal = () => {
+
         modalRef.current?.showModal()
+
+        getFocusableElements(modalRef.current)
+
+
     }
 
     const closeModal = () => {
         modalRef.current?.close()
+
     }
 
     const openZoomedPicture = (clickedPictureSrc) => {
         zoomedPictureRef.current?.showModal()
-        console.log(zoomedPictureRef.current.lastChild.src)
+
+
+        getFocusableElements(zoomedPictureRef.current)
+
         zoomedPictureRef.current.lastChild.src = clickedPictureSrc
     }
     const closeZoomedPicture = () => {
         zoomedPictureRef.current?.close()
+        modalRef.current?.showModal()
     }
+
 
     return (
         <div className="projectCard">
@@ -35,16 +62,67 @@ export default function ProjectCard({ projectUrl, projectName, backgroundImgUrl,
                 <div className="stack">{projectStack.map(techIconSrc => <img src={techIconSrc} alt="icone de langage informatique" key={projectStack.indexOf(techIconSrc)} className="tech_icon" />)}</div>
             </div>
             <button className="show_modal_button" onClick={openModal} >En savoir plus ...</button>
-            <dialog ref={modalRef} className="project_modal" closedby="any" >
-                <button className="close_modal_button" onClick={closeModal}>Revenir au portfolio</button>
+            <dialog ref={modalRef} className="project_modal" closedby="any" onKeyDown={(e) => {
+                if (e.key !== "Tab") return
+                e.preventDefault()
+
+                let index = focusableElements.findIndex(f => f === modalRef.current.querySelector(":focus"))
+                getFocusableElements(modalRef.current)
+                if (e.shiftKey === true) {
+                    index--
+
+                } else {
+                    index++
+
+                }
+                if (index >= focusableElements.length) {
+                    index = 0
+
+                }
+                if (index < 0) {
+                    index = focusableElements.length - 1
+
+                }
+
+                focusableElements[index].focus()
+            }} >
+                <button className="close_modal_button" onClick={closeModal}  >Revenir au portfolio</button>
                 <div className="project_description">
                     <p>{projectDescription}</p>
                 </div>
                 <div className="modal_gallery">
-                    {projectModalPictures.map(picturePath => <img src={picturePath} alt={`screenshot du projet ${projectName}`} className="modal_gallery_img" key={projectModalPictures.indexOf(picturePath)} onClick={(e) => openZoomedPicture(e.currentTarget.src)} />)}
+                    {projectModalPictures.map(picturePath => <img src={picturePath} alt={`screenshot du projet ${projectName}`} className="modal_gallery_img" key={projectModalPictures.indexOf(picturePath)} onClick={(e) => openZoomedPicture(e.currentTarget.src)} tabIndex={0} onKeyDown={(e) => {
+                        if (e.key === "Enter" || e.key === ' ') {
+                            e.preventDefault()
+                            openZoomedPicture(e.currentTarget.src)
+                        }
+                    }} />)}
                 </div>
             </dialog>
-            <dialog className="picture_modal_zoom" ref={zoomedPictureRef} closedby="any">
+            <dialog className="picture_modal_zoom" ref={zoomedPictureRef} closedby="any" onKeyDown={(e) => {
+                if (e.key !== "Tab") return
+                e.preventDefault()
+
+                let index = focusableElements.findIndex(f => f === zoomedPictureRef.current.querySelector(":focus"))
+                getFocusableElements(zoomedPictureRef.current)
+                if (e.shiftKey === true) {
+                    index--
+
+                } else {
+                    index++
+
+                }
+                if (index >= focusableElements.length) {
+                    index = 0
+
+                }
+                if (index < 0) {
+                    index = focusableElements.length - 1
+
+                }
+
+                focusableElements[index].focus()
+            }}>
                 <button className="close_modal_button" onClick={closeZoomedPicture}>Revenir au projet</button>
                 <img className="zoomed_picture" ></img>
             </dialog>
